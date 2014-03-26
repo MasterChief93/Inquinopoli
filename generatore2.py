4cb612a4aec95e07fffda5ac87b37fc72c15be71
Bredduz <sirbredduz@yahoo.it>
2014-03-26 10:27:27 +0100

    Bellman Ford funzionante (???)

--- generatore2.py
+++ generatore2.py
@@ -4,7 +4,6 @@ from random import randrange
 
 def generatore(): # questo generatore crea grafi ottimali per provare l'algoritmo
     n=10
-    m=n*(n-1)
     nodelist=[]
     archlist=[]
     for i in range(1,n+1):
@@ -16,11 +15,34 @@ def generatore(): # questo generatore crea grafi ottimali per provare l'algoritm
             if x!=y:
                 arch=[x,y]
                 archlist.append(arch)
+    pm20list = []
+    for elem in nodelist:
+        pm20list.append(elem[1])
+
+    return [10,pm20list,archlist]
+
+
+#print generatore()
+
+def Generatore():
+    n=10
+    nodelist=[]
+    archlist=[]
+    pol=1
+    for i in range(1,n+1):
+        node=[i,pol]
+        nodelist.append(node)
+        pol+=1
+    for x in range(1,n+1):
+        for y in range(1,n+1):
+            if x!=y:
+                arch=[x,y]
+                archlist.append(arch)
+    pm20list = []
+    for elem in nodelist:
+        pm20list.append(elem[1])
     print nodelist
-    return archlist
+    return [10,pm20list,archlist]
 
 
-print generatore()
-
-
-
+Generatore()
